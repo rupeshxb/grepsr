0ac4bd48f9e324d7cc133d73f4e4c04ba91f33a4
Rupesh Bhatta <rupesh.bhatta1234@gmail.com>
2023-04-16 17:49:30 +0545

    done

--- question_1_a.php
+++ question_1_a.php
@@ -2,30 +2,77 @@
 
 <?php
 $data = "clusters";
-if(isset($data)){
+if (isset($data)) {
     echo "The variable \$data is set and not NULL. \n";
-}
-else{
+} else {
     echo "The variable \$data is not set and/or not NULL. \n";
 }
 
 $name;
-if(isset($name)){
+if (isset($name)) {
     echo "The variable \$name is set and not NULL \n";
-}
-else{
+} else {
     echo "The variable \$name is not set and/or NULL. \n";
 }
 
 $email = null;
-if(isset($email)){
+if (isset($email)) {
     echo "The variable \$email is set and not null. \n";
-}
-else{
+} else {
     echo "The variable \$email is set and NULL. \n";
 }
 ?>
 
+<!-- empty() function -->
+
+<?php
+$counter = null;
+if (empty($counter)) {
+    echo "The variable \$counter is empty. \n";
+} else {
+    echo "The variable \$counter is not empty. \n";
+}
+
+$name = '';
+if (empty($name)) {
+    echo "The variable \$name is empty. \n";
+} else {
+    echo "The variable \$name is not empty.";
+}
+
+$value = 0.0;
+if (empty($value)) {
+    echo "The variable \$value is empty. \n";
+} else {
+    echo "The variable \$value is not empty. \n";
+}
+
+$status = false;
+if (empty($status)) {
+    echo "The variable \$status is empty. \n";
+} else {
+    echo "The variable \$status is not empty. \n";
+}
+
+$myArray = array();
+if (empty($myArray)) {
+    echo "The variable \$myArray is empty. \n";
+} else {
+    echo "The variable \$myArray is not empty. \n";
+}
+?>
+
+<!-- array_map() function -->
 
 
+<?php
+$cube_func = function ($n) {
+    return $n * $n * $n;
+};
 
+$numbers = array(1, 2, 3, 4, 5);
+
+$cubed_numbers = array_map($cube_func, $numbers);
+
+print_r($cubed_numbers);
+?>
